2945c85d85273bca0c050fde0aa77c2e6d420ef2
Benjamin Smith <Facade.Developer@gmail.com>
2015-07-08 17:28:49 -0500

    Initial Minecraft Files

---
+++ minecraft/random/facades/ForgeUnisCommand.java
@@ -0,0 +1,78 @@
+package random.facades;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.common.DimensionManager;
+
+public class ForgeUnisCommand extends CommandBase
+{
+
+	@Override
+	public String getCommandName()
+	{
+		return "fuc";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender icommandsender)
+	{
+		return "fuc <options>";
+	}
+
+	@Override
+	public void processCommand(ICommandSender icommandsender, String[] astring)
+	{
+		EntityPlayerMP plyr = null;
+		if(icommandsender instanceof EntityPlayerMP)
+		{
+			plyr = (EntityPlayerMP)icommandsender;
+			if(astring.length == 0)
+			{
+				plyr.addChatMessage("Dimension: "+plyr.dimension);
+			}
+			if(astring.length == 2 && isInteger(astring[1]))
+			{
+				if(astring[0].equals("tp"))
+				{
+					int dim = Integer.parseInt(astring[1]);
+					System.out.println("fdc tp "+ dim);
+					plyr.mcServer.getConfigurationManager().transferPlayerToDimension(plyr, dim);
+					plyr.timeUntilPortal = plyr.getPortalCooldown();
+				}
+			} else if(astring.length == 3 && isInteger(astring[1]) && isInteger(astring[2]))
+			{
+				 if(astring[0].equals("create"))
+				{
+					int a = Integer.parseInt(astring[1]), b = Integer.parseInt(astring[2]);
+					System.out.println("fdc create "+ a +" " + b);
+					DimensionManager.registerDimension(a, b);
+				}
+			}
+		}
+	}
+	
+	public static boolean isInteger(String str) {
+		if (str == null) {
+			return false;
+		}
+		int length = str.length();
+		if (length == 0) {
+			return false;
+		}
+		int i = 0;
+		if (str.charAt(0) == '-') {
+			if (length == 1) {
+				return false;
+			}
+			i = 1;
+		}
+		for (; i < length; i++) {
+			char c = str.charAt(i);
+			if (c <= '/' || c >= ':') {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ minecraft/random/facades/ForgeUnisMod.java
@@ -0,0 +1,80 @@
+package random.facades;
+
+import java.io.File;
+import java.lang.reflect.Field;
+
+import net.minecraft.command.ServerCommandManager;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.DimensionManager;
+import net.minecraftforge.common.Property;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid=ForgeUnisMod.MODID, name=ForgeUnisMod.MODNAME, version=ForgeUnisMod.MODVER) //Tell forge "Oh hey, there's a new mod here to load."
+public class ForgeUnisMod
+{
+    //Set the ID of the mod (Should be lower case).
+    public static final String MODID = "forge_uni";
+    //Set the "Name" of the mod.
+    public static final String MODNAME = "Forge Universe Mod";
+    //Set the version of the mod.
+    public static final String MODVER = "0.0.0";
+
+    @Instance(value = ForgeUnisMod.MODID) //Tell Forge what instance to use.
+    public static ForgeUnisMod instance;
+       
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+    	Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+
+    	config.load();
+    	File dir;
+		try
+		{
+			Loader ld = Loader.instance();
+			Field privie = Loader.class.getDeclaredField("canonicalModsDir");
+			privie.setAccessible(true);
+			dir = (File)(privie.get(ld));
+			int id = -700;
+			for(File ls : dir.listFiles())
+			{
+				if(ls.getName().startsWith("uni_"))
+				{
+					String nm = ls.getName().substring(4);
+					System.out.println(nm);
+					while(DimensionManager.isDimensionRegistered(id)) id--;
+			    	Property val = config.get(nm, "nt_id", id);
+			    	DimensionManager.registerDimension(val.getInt(), -1);
+					while(DimensionManager.isDimensionRegistered(id)) id--;
+			    	val = config.get(nm, "ow_id", id);
+			    	DimensionManager.registerDimension(val.getInt(), 0);
+					while(DimensionManager.isDimensionRegistered(id)) id--;
+			    	val = config.get(nm, "end_id", id);
+			    	DimensionManager.registerDimension(val.getInt(), 1);
+				}
+			}
+		} catch (Exception e)
+		{ e.printStackTrace(); }
+		
+    	config.save();
+    }
+       
+    @EventHandler
+    public void load(FMLInitializationEvent event)
+    {
+    }
+       
+    @EventHandler
+    public void serverStart(FMLServerStartingEvent event)
+    {
+		ServerCommandManager abc = (ServerCommandManager)(MinecraftServer.getServer().getCommandManager());
+		abc.registerCommand(new ForgeUnisCommand());
+    }
+}
